2439837267ea324dbbae739b897b0b8ec0390e3e
pritibakale <bakalepriti1@gmail.com>
2020-02-03 18:15:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-jsplumb-test-pznwju
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jsplumb-test-pznwju)
---
+++ app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, AfterContentInit, OnInit } from '@angular/core';
+import { NodeService } from './node.service';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  nodes = [];
+
+  connections = [];
+
+  constructor(private nodeService: NodeService) {}
+  
+  ngOnInit() {
+    //  this.fillFromJson();
+   
+  }
+
+  fillFromJson() {
+    const json = `{"nodes":[{"id":"Step_0 id: b46a17","top":177,"left":146},{"id":"Step_1 id: efd2ce","top":302,"left":130},{"id":"Step id_2eb091","top":41,"left":158}],"connections":[{"uuids":["Step_0 id: b46a17_bottom","Step_1 id: efd2ce_top"]},{"uuids":["Step id_2eb091_bottom","Step_0 id: b46a17_top"]}]}`;
+    const data = JSON.parse(json);
+   
+    this.nodes = data.nodes;
+    this.connections = data.connections;
+  }
+
+}
+
